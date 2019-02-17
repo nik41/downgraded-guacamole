c24eb2bd57e8d25eb5f52e0254a1e8cf005e4b61
nik41 <47713243+nik41@users.noreply.github.com>
2019-02-17 13:24:17 +0200

    Add files via upload

---
+++ project3.py
@@ -0,0 +1,25 @@
+import os
+
+def removeCommentsFromFile(inputFile):
+
+  targetFile = open("test2.py", "w")
+  nameOfInputFile = inputFile
+  commentIdentifier = "#"
+  file = open(inputFile, "r+")
+
+  for line in file:
+    commentSection = line.find(commentIdentifier)
+    if commentSection >= 0:
+      line = line[:commentSection]
+
+    targetFile.write(line + "\n")
+
+  file.close()
+  targetFile.close()
+
+  os.remove(inputFile)
+  os.renames("test2.py", nameOfInputFile)
+
+
+removeCommentsFromFile("project3PythonCodeWithCommentsToBeRemoved.py")
+
